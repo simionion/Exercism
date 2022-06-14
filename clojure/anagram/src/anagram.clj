487e24d8c766b7ead8f58f07b46e82369f2f3606
Simion Ion <52240502+simionion@users.noreply.github.com>
2022-06-14 12:46:37 +0300

    Anagram

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,10 +1,8 @@
-(ns anagram)
+(ns anagram
+  (:require [clojure.string :refer [lower-case] :rename {lower-case lc}]))
 
-(defn anagrams-for [word prospect-list] ;; <- arglist goes here
-  (->> (clojure.string/join " " prospect-list)
-    (re-matches (
-              re-pattern 
-              (format "[%s]{%d}" word)
-              (count word)))
-    )
-)
+(defn anagrams-for [word prospect-list]
+  (let [hasher #(reduce + (map int (lc %1)))]
+    (->> (remove #(= (lc %1) (lc word)) prospect-list)
+         (filter #(= (hasher %1) (hasher word)))
+         )))
