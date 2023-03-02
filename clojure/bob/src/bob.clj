024efa8000cbf7680be5b485a3208cefb0e03c14
Simion Ion <52240502+simionion@users.noreply.github.com>
2023-03-02 13:29:37 +0200

    Bob

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -2,15 +2,14 @@
 
 (def reg-rsp
   {
-   #"^[^a-z]*[A-Z]+[^a-z]*\?$" "Calm down, I know what I'm doing!"
-   #"^[^a-z]*[A-Z]+[^a-z]*$"   "Whoa, chill out!"
-   #"\?\s*$"                   "Sure."
-   #"\w+"                      "Whatever."
+
    }
   )
 
 (defn response-for [s]
-  (-> (fn [rez [reg rsp]]
-        (or rez (when (not-empty (re-find reg s)) rsp)))
-      (reduce nil reg-rsp)
-      (or "Fine. Be that way!")))
+  (condp (comp s re-find not-empty) s
+    #"^[^a-z]*[A-Z]+[^a-z]*\?$" "Calm down, I know what I'm doing!"
+    #"^[^a-z]*[A-Z]+[^a-z]*$" "Whoa, chill out!"
+    #"\?\s*$" "Sure."
+    #"\w+" "Whatever."
+    ))
